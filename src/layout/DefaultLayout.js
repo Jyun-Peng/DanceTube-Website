06e96b68a1e37d93253443bbfa49e2836bf160c5
Jenniferlala <kekvkikn@gmail.com>
2022-01-12 03:16:20 +0800

    Make the Navbar fixed

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -1,10 +1,10 @@
 import react from 'react';
 import NavBar from '../components/NavBar';
 
-function DefaultLayout({ children }) {
+function DefaultLayout({ children, dropdown }) {
     return (
-        <div className="bg-black mx-auto max-w-xs">
-            <NavBar />
+        <div className="bg-black mx-auto max-w-xs pt-[4.25rem]">
+            <NavBar dropdown={dropdown} />
             {children}
         </div>
     );

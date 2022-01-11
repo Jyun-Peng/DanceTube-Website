77c994e075c654364ad890e62401c6a606b4f310
Jenniferlala <kekvkikn@gmail.com>
2022-01-12 03:15:48 +0800

    Add Home page layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,37 @@
-import react from 'react';
+import react, { useEffect, useState } from 'react';
 import DefaultLayout from '../layout/DefaultLayout';
+import { searchVideo } from '../functional/SearchVideo';
+import { Link } from 'react-router-dom';
+import Button from '../components/Button';
+import Carousel from '../components/Carousel';
+
+function Section({ title }) {
+    const [data, setData] = useState([]);
+    const keyword = title.toLowerCase();
+
+    useEffect(() => {
+        // searchVideo(keyword, null, setData);
+    }, []);
+
+    return (
+        <div className="pb-7">
+            <div className="flex justify-between items-center px-4 py-2 mb-4">
+                <h3 className="text-xl text-white font-bold">{title}</h3>
+                <Button text="more" search={title.toLowerCase()} />
+            </div>
+            <Carousel />
+        </div>
+    );
+}
 
 function Home() {
-    return <DefaultLayout>homepage</DefaultLayout>;
+    return (
+        <DefaultLayout>
+            <Section title="Locking" />
+            <Section title="Popping" />
+            <Section title="Waacking" />
+        </DefaultLayout>
+    );
 }
 
 export default Home;

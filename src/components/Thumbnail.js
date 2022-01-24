2f0e1b32d809224077bb538ed58182983cd3ffe6
Jenniferlala <kekvkikn@gmail.com>
2022-01-24 17:48:44 +0800

    Adjust Thumbnail style

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -4,22 +4,43 @@ import styled from 'styled-components';
 
 const StyledContainer = styled.div`
     width: 22rem;
-    height: 15rem;
     flex: 1 0 auto;
     border-radius: 0.3rem;
     overflow: hidden;
     box-shadow: 0px 4px 6px 0px rgba(0, 0, 0, 0.2);
 `;
 
-function Thumbnail({ imageURL = '', title = '', videoId = '' }) {
+const ThumbnailTitle = styled.div`
+    padding: 1rem;
+    background-color: #545454;
+
+    & > h3 {
+        height: 2.4rem;
+        font-size: 1rem;
+        font-weight: 600;
+        line-height: 1.2;
+        color: #fefefe;
+        overflow: hidden;
+    }
+
+    & > p {
+        font-size: 0.9rem;
+        color: #dddddd;
+        margin-bottom: 0.25rem;
+    }
+`;
+
+function Thumbnail({ imageURL = '', title = '', videoId = '', publishedDate = '' }) {
     console.log(imageURL);
+    publishedDate = publishedDate.split('T')[0];
     return (
         <Link to={`/player/${videoId}`} style={{ display: 'inline-block' }}>
             <StyledContainer>
-                <img src={imageURL} alt="image" className="w-full" />
-                <div className="bg-white text-base max-h-[3.5rem] absolute bottom-0 px-4 py-3 bg-gray text-white font-bold">
-                    {title}
-                </div>
+                <img src={imageURL} alt="image" className="w-full h-[12rem] object-cover object-center" />
+                <ThumbnailTitle>
+                    <p>{`發布日期：${publishedDate}`}</p>
+                    <h3>{title}</h3>
+                </ThumbnailTitle>
             </StyledContainer>
         </Link>
     );

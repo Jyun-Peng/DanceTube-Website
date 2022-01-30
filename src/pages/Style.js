d1815df11103ed340bb40a5c90d99a29658c9880
Jenniferlala <kekvkikn@gmail.com>
2022-01-30 14:59:24 +0800

    Add thumbnails to style page

--- src/pages/Style.js
+++ src/pages/Style.js
@@ -1,8 +1,39 @@
 import react from 'react';
-import DefaultLayout from '../layout/DefaultLayout';
+import { useState, useEffect } from 'react/cjs/react.development';
+import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 
+import DefaultLayout from '../layout/DefaultLayout';
+import { searchVideo } from '../functional/SearchVideo';
+import BigThumbnail from '../components/BigThumbnail';
+
+const VerticalFlexBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    gap: 1rem;
+`;
 function Style() {
-    return <DefaultLayout>Style</DefaultLayout>;
+    const [videoList, setVideoList] = useState([]);
+    let { keyword } = useParams();
+
+    useEffect(() => searchVideo(keyword, null, setVideoList), []);
+
+    return (
+        <DefaultLayout>
+            <VerticalFlexBox>
+                {videoList.map((video) => (
+                    <BigThumbnail
+                        imageURL={video.snippet.thumbnails.high.url}
+                        title={video.snippet.title}
+                        videoId={video.id.videoId}
+                        publishedDate={video.snippet.publishedAt}
+                    />
+                ))}
+            </VerticalFlexBox>
+        </DefaultLayout>
+    );
 }
 
 export default Style;

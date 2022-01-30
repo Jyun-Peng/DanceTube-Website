44064d6348630503a6a975d9f99e7e7594f292b0
Jenniferlala <kekvkikn@gmail.com>
2022-01-31 00:36:40 +0800

    Adjust padding-top of default layout

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -3,9 +3,9 @@ import NavBar from '../components/NavBar';
 
 function DefaultLayout({ children, dropdown }) {
     return (
-        <div className="bg-black mx-auto  pt-[4.25rem] overflow-hidden">
+        <div className="bg-black mx-auto  overflow-hidden">
             <NavBar dropdown={dropdown} />
-            {children}
+            <div className="max-w-4xl mx-auto pt-[2.5rem] pb-[4.25rem]">{children}</div>
         </div>
     );
 }

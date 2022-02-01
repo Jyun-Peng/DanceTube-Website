78b1f2178204d9d1251a098e5e2f02a543447db4
Jenniferlala <kekvkikn@gmail.com>
2022-02-02 02:45:52 +0800

    Add overflow scroll to each group of thumbnails.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,36 +1,47 @@
 import react, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import DefaultLayout from '../layout/DefaultLayout';
-import { searchVideo } from '../functional/SearchVideo';
-import { Link } from 'react-router-dom';
-import Button from '../components/Button';
+
+import DefaultContainer from '../layout/DefaultContainer';
 import Carousel from '../components/Carousel';
 import Thumbnail from '../components/Thumbnail';
-import DefaultContainer from '../layout/DefaultContainer';
+import Button from '../components/Button';
+import { searchVideoList } from '../functional/GetData';
+import FlexBox from '../layout/FlexBox';
+
+const ThumbnailContainer = styled.div`
+    flex-shrink: 0;
+    width: 22rem;
+`;
 
 function Section({ title }) {
     const [videoList, setVideoList] = useState([]);
     const keyword = title.toLowerCase();
 
-    useEffect(() => searchVideo(keyword, null, setVideoList), []);
+    //Fetch youtube video
+    useEffect(() => searchVideoList(keyword, null, setVideoList), []);
 
     return (
-        <div className="pb-7">
+        <div className="pt-4">
             <DefaultContainer>
-                <div className="flex justify-between items-center px-4 py-2 mb-4">
+                <div className="flex justify-between items-center py-2 mb-4">
                     <h3 className="text-xl text-white font-bold">{title}</h3>
-                    <Button text="more" search={title.toLowerCase()} />
+                    <Button text="more" keyword={title.toLowerCase()} />
                 </div>
 
-                <div>
-                    {videoList.length > 0 && (
-                        <Thumbnail
-                            imageURL={videoList[0].snippet.thumbnails.high.url}
-                            title={videoList[0].snippet.title}
-                            videoId={videoList[0].id.videoId}
-                            publishedDate={videoList[0].snippet.publishedAt}
-                        />
-                    )}
-                </div>
+                <FlexBox className="overflow-x-scroll">
+                    {videoList.map((video) => (
+                        <ThumbnailContainer>
+                            <Thumbnail
+                                imageURL={video.snippet.thumbnails.high.url}
+                                title={video.snippet.title}
+                                videoId={video.id.videoId}
+                                publishedDate={video.snippet.publishedAt}
+                                keyword={keyword}
+                            />
+                        </ThumbnailContainer>
+                    ))}
+                </FlexBox>
             </DefaultContainer>
         </div>
     );

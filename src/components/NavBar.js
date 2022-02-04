990a3adca5463932e102e3ff0c55ea5df31044d6
Jenniferlala <kekvkikn@gmail.com>
2022-02-04 17:50:05 +0800

    Add style dropdown to navbar.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,108 @@
-import react from 'react';
+import react, { useState } from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import DefaultContainer from '../layout/DefaultContainer';
+import FlexBox from '../layout/FlexBox';
+import Button from './Button';
+import arrow from '../images/Logo/arrow-down.svg';
 
 const StyledHeader = styled.header`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0.5rem 0;
     box-shadow: 0px 4px 6px 0px rgba(0, 0, 0, 0.2);
+    padding: 0.5rem 0;
     position: fixed;
     top: 0;
-    width: 100vw;
+    width: 100%;
     background-color: #333333;
     z-index: 10;
 `;
+
+const StyledLogo = styled.h3`
+    font-size: 1.75rem;
+    color: var(--white);
+    font-weight: 600;
+`;
+
+function Dropdown({ currentText, list }) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleClick = function () {
+        setIsOpen(!isOpen);
+    };
+
+    const ListContainer = styled.div`
+        position: absolute;
+        top: 2.25rem;
+        right: 0;
+        background-color: var(--white);
+        border-radius: 0.25rem;
+        overflow: hidden;
+
+        & li > a {
+            display: block;
+            width: 100%;
+            padding: 0.5rem 1.25rem 0.5rem 0.5rem;
+            font-size: 1rem;
+            font-weight: 600;
+
+            &:hover {
+                background-color: var(--gray-text);
+            }
+        }
+    `;
+
+    const ArrowSvg = styled.img`
+        width: 1rem;
+        transform: ${(props) => (props.isOpen ? 'rotate(180deg)' : 'rotate(0deg)')};
+    `;
+
+    return (
+        <div>
+            <Button
+                text={
+                    <FlexBox gap="0.5rem">
+                        <span>{currentText}</span>
+                        <ArrowSvg src={arrow} isOpen={isOpen} />
+                    </FlexBox>
+                }
+                handleClick={handleClick}
+                bgColor={'--gray-bg'}
+            />
+            {isOpen && (
+                <ListContainer onClick={() => setIsOpen(!isOpen)}>
+                    <ul>
+                        {list.map((ele) => (
+                            <li>
+                                <Link to={ele.url}>{ele.text}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </ListContainer>
+            )}
+        </div>
+    );
+}
 function NavBar({ dropdown }) {
+    let { style } = useParams();
+    const styleList = [
+        { text: 'Locking', url: '/style/locking' },
+        { text: 'popping', url: '/style/popping' },
+        { text: 'waacking', url: '/style/waacking' },
+    ];
+
     return (
         <StyledHeader>
             <DefaultContainer>
-                <Link to="/">
-                    <h3 className="text-[1.5rem] text-white font-bold">DanceTube</h3>
-                </Link>
+                <FlexBox spaceBetween>
+                    <Link to="/">
+                        <StyledLogo>DanceTube</StyledLogo>
+                    </Link>
 
-                <div className="flex">{dropdown && <div className="text-white">style</div>}</div>
+                    <div className="flex">
+                        {dropdown && (
+                            <Dropdown currentText={style[0].toUpperCase() + style.slice(1)} list={styleList} />
+                        )}
+                    </div>
+                </FlexBox>
             </DefaultContainer>
         </StyledHeader>
     );

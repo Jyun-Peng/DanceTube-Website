1205a95682deea986020cf1e85a44fcc8f5a5084
Jenniferlala <kekvkikn@gmail.com>
2022-02-04 17:52:46 +0800

    Add customizable gap and to flex box.

--- src/layout/FlexBox.js
+++ src/layout/FlexBox.js
@@ -2,8 +2,9 @@ import styled from 'styled-components';
 
 const FlexBox = styled.div`
     display: flex;
-    gap: 1rem;
+    gap: ${(props) => (props.gap ? props.gap : '0')};
     justify-content: ${(props) => (props.spaceBetween ? 'space-between' : 'flex-start')};
+    align-items: center;
 `;
 
 export default FlexBox;

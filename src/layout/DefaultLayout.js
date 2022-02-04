987419363f82e6350efa656808493fa5a193527a
Jenniferlala <kekvkikn@gmail.com>
2022-02-04 17:51:38 +0800

    Isolate wrapper from default layout.

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -1,12 +1,20 @@
 import react from 'react';
+import styled from 'styled-components';
 import NavBar from '../components/NavBar';
+
+const StyledWrapper = styled.div`
+    background-color: var(--black);
+    overflow-x: hidden;
+    width: 100%;
+    min-height: 100vh;
+`;
 
 function DefaultLayout({ children, dropdown }) {
     return (
-        <div className="bg-black mx-auto  overflow-hidden">
+        <StyledWrapper>
             <NavBar dropdown={dropdown} />
             <div className="max-w-4xl mx-auto pt-[2.5rem] pb-[4.25rem]">{children}</div>
-        </div>
+        </StyledWrapper>
     );
 }
 

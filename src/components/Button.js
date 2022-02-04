6a9310b613c12818daeaf017e65a3d73c84e8910
Jenniferlala <kekvkikn@gmail.com>
2022-02-04 17:50:34 +0800

    Adjust butto font size.

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const StyledButton = styled.button`
-    font-size: 0.875rem;
+    font-size: 1rem;
     font-weight: 600;
     color: var(--white);
     background-color: var(${(props) => (props.bgColor ? props.bgColor : '--purple')});
 
     line-height: 1;
     padding: 0.375rem 0.75rem;
-    border-radius: 0.8125rem;
+    border-radius: 0.875rem;
 `;
 function Button({ text, url, handleClick, bgColor }) {
     return (

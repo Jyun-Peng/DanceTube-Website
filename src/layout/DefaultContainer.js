8c34aabc6988b2450c2eb8a4e65f57a1fa3cc123
Jenniferlala <kekvkikn@gmail.com>
2022-02-08 18:20:42 +0800

    Adjust DefaultContainer.

--- src/layout/DefaultContainer.js
+++ src/layout/DefaultContainer.js
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 const DefaultContainer = styled.div`
     padding: 0 1rem;
+    max-width: 56rem;
+    margin: 0 auto;
 `;
 
 export default DefaultContainer;

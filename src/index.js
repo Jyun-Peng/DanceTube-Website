275dcff46ae22bc42cc9f5c56eaa0950d7c12ba4
Jenniferlala <kekvkikn@gmail.com>
2022-02-13 07:04:18 +0800

    Add ScrollToTop function.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './functional/ScrollToTop';
 
 ReactDOM.render(
     <BrowserRouter>
+        <ScrollToTop />
         <App />
     </BrowserRouter>,
     document.getElementById('root')
